ded192bc44f3d52aca988d127601e199849a35ec
Kate Varenik <katyavarenik@mail.ru>
2022-05-30 22:23:11 +0300

    change router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import "./reset.css";
 import "./style.css";
